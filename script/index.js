66abf0142a2d1364e3585c13788d430ee3a39a47
andyjohn23 <andrewnick906@gmail.com>
2020-08-21 15:16:40 +0300

    updated files

--- script/index.js
+++ script/index.js
@@ -9,17 +9,17 @@ function validateform(){
     if((yearofbirth == "") || (yearofbirth.length !=4) || 
     (yearofbirth < 1900) || ( yearofbirth > current_year)) {
         alert("please provide a valid year of birth between 1900-2020");
-        document.myForm.yearofbirth.focus() ;
+       
         return false;
 
     }else if(monthofbirth == "" || monthofbirth.length !=2 || monthofbirth > 12) {
             alert("please provide a valid month of birth between 1-12");
-            document.myForm.monthofbirth.focus() ;
+           
             return false;
 
     }else if(dateofbirth == "" || dateofbirth.length !=2 || dateofbirth > 31) {
             alert("please provide a valid date of birth between 1-31");
-            document.myForm.dateofbirth.focus() ;
+            
             return false;
 
     }else if(gender[0].checked==false && gender[1].checked==false){
